08b4a486edeb4dee62d5dc3bfa38a738142d4c2d
Keith Burgie <krburgie@gmail.com>
2019-04-27 04:12:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array)
+  # show customer their current place in line
+  # if nobody, "The line is currently empty"
+end
+
+def take_a_number(deliName, customerName)
+  # puts the person's name and their position in line
+end
+
+def now_serving
+  # puts next person in line
+    # removes them from the front 
+  # if no one in line 
+    # puts "There is nobody waiting to be served!"
+end
+
+line(katz_deli)
