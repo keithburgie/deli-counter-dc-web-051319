204bcd405a0a211d121d1d85aac79a997b21eff0
Keith Burgie <krburgie@gmail.com>
2019-04-27 04:13:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,9 @@ katz_deli = []
 def line(array)
   # show customer their current place in line
   # if nobody, "The line is currently empty"
+  if line.length == 0 
+    puts "The line is currently empty."
+  end
 end
 
 def take_a_number(deliName, customerName)
