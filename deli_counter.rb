3020192f0c8bc59d39904629d80b2edfa193e58c
Keith Burgie <krburgie@gmail.com>
2019-04-27 04:36:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,37 @@
 # Write your code here.
 
 katz_deli = ["Keith", "Jeff", "Isaac"]
+new_deli = []
 
-def line(array)
+def line(deli)
   # show customer their current place in line
   # if nobody, "The line is currently empty"
-  if array.length == 0 
+  if deli.length == 0 
     puts "The line is currently empty."
   else
     i = 1
     line = []
-    array.each do |person|
+    deli.each do |person|
         line.push("#{i}. #{person}")
         i += 1
     end
     puts "The line is currently: #{line.join(" ")}"
   end
 end
-line(katz_deli)
 
-def take_a_number(deliName, customerName)
+
+def take_a_number(deli, customer)
   # puts the person's name and their position in line
+  position = deli.length
+  deli.push(customer)
+  puts "Welcome, #{customer}. You are number #{position} in line."
   
-  #take_a_number there is nobody in line should add a person tothe line
-  #take_a_number there are already people in line should add a person to the end of the line
-  #take_a_number adding multiple people in a row should correctly build the line
 end
+take_a_number(new_deli, "Garth")
+take_a_number(new_deli, "Shamus")
+take_a_number(new_deli, "Teresa")
+
+line(new_deli)
 
 def now_serving
   # puts next person in line
