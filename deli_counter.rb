54b0f7ab07638c5a47adbf0dcee75d02128a119d
Keith Burgie <krburgie@gmail.com>
2019-04-27 04:48:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,8 @@
-# Write your code here.
-
 katz_deli = ["Keith", "Jeff", "Isaac"]
 new_deli = []
 
+# show customer their current place in line
 def line(deli)
-  # show customer their current place in line
-  # if nobody, "The line is currently empty"
   if deli.length == 0 
     puts "The line is currently empty."
   else
@@ -19,31 +16,19 @@ def line(deli)
   end
 end
 
-
+# puts the person's name and their position in line
 def take_a_number(deli, customer)
-  # puts the person's name and their position in line
   position = deli.length + 1
   deli.push(customer)
   puts "Welcome, #{customer}. You are number #{position} in line."
-  
-  #take_a_number there is nobody in line should add a person tothe line
-  #take_a_number there are already people in line should add a person to the end of the line
-  #take_a_number adding multiple people in a row should correctly build the line
 end
-take_a_number(new_deli, "Garth")
-take_a_number(new_deli, "Shamus")
-take_a_number(new_deli, "Teresa")
 
-line(new_deli)
 
 def now_serving(deli)
   if deli.length == 0 
     puts "There is nobody waiting to be served!"
   else
-  #now_serving there are no people in line should say that the line is empty
-  #now_serving there are people in line should serve the first person in line and remove them from the queue
-  # puts next person in line
-    # removes them from the front 
-  # if no one in line 
-    # puts "There is nobody waiting to be served!"
+    puts "Currently serving #{deli[0]}"
+    deli.shift()
+  end
 end
